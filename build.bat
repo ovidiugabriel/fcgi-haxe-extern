9a2059c9307f0478f05a3048bc36e67d18801762
Ovidiu <ovidiugabriel@gmail.com>
2016-11-10 23:41:07 +0200

    added initial revision

---
+++ build.bat
@@ -0,0 +1,15 @@
+:: Need to have MSVC installed and run this script from command line
+:: Tested in cygwin also
+:: rem set PATH=%PATH%;C:\HaxeToolkit\neko
+
+IF EXIST Server\src\cpp GOTO ALREADYEXISTS
+mkdir Server\src\cpp
+:ALREADYEXISTS
+
+copy cpp\Web.h Server\src\cpp
+cd haxe
+haxe build.hxml
+cd ..
+copy Server\Server.exe C:\xampp\cgi-bin
+Server\Server.exe
+
