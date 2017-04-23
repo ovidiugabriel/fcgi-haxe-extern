d51632fbbff809095a5bfd717656d026a8057ecf
Ovidiu <ovidiugabriel@gmail.com>
2017-04-23 23:30:19 +0300

    Temporary changes in Dialog.hx server side

--- haxe/Dialog.hx
+++ haxe/Dialog.hx
@@ -4,6 +4,8 @@
  * Recommended presentation: https://www.youtube.com/watch?v=k2rw7-uL6RU
  */
 
+import haxe.ds.StringMap;
+
 #if cpp
 // Available only with a C++11 compiler
 /*
@@ -13,17 +15,31 @@
      </files>
 ")
 */
-@:headerCode('#include "TDialog.h"')
+// @:headerCode('#include "TDialog.h"')
 #end
 class Dialog {
     public function new() {}
 
-    public function div( x : Float, y : Float ) : Float {
+    public function div( x : Float, y : Float ) : Array<StringMap<Dynamic>> {
+        /*
         #if cpp
             untyped __cpp__('Result<double> result = TDialog::div(x, y)');
             return cpp.CatchException.getResult();
         #else
-            return x / y;
-        #end
+        */
+
+        if (0 == y) {
+            throw "division by zero";
+        }
+        var result = new Array<StringMap<Dynamic>>();
+        var row = new StringMap<Dynamic>();
+        row.set("div", x / y);
+        result.push(row);
+
+        row = new StringMap<Dynamic>();
+        row.set("div", x / y);
+        result.push(row);
+        return result;
+    
     }
 }
