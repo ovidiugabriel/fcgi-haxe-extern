b2f7e553b918a49b9e275b86d9a8f549f72e182c
Ovidiu <ovidiugabriel@gmail.com>
2017-12-03 11:09:51 +0200

    Converted build-cpp-client.bat to build-cpp-client.sh

---
+++ client/build-cpp-client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+export FCGI_HAXE_EXTERN_PATH=`dirname $(cygpath $PWD)`
+export HXCPP_MINGW=1
+
+if [ "" == "$FCGI_HAXE_EXTERN_PATH" ] ; then
+    echo "FCGI_HAXE_EXTERN_PATH is not set"
+    exit 1
+fi
+
+haxe -main Main Client NativeClient -D HXCPP_M32 -D HXCPP_STACK_TRACE -D HXCPP_STACK_LINE -cpp output/Client
+# -D static_link
