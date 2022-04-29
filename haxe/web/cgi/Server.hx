7bcf3d1ba9e88acbd1175d4f5aa29628460b7b70
Ovidiu <ovidiugabriel@gmail.com>
2022-04-29 17:14:07 +0300

    Create Server.hx

---
+++ haxe/web/cgi/Server.hx
@@ -0,0 +1,19 @@
+
+package web.cgi;
+
+class Server {
+    /** Applies HTML encoding to the specified string. **/
+    public function HTMLEncode(input : String) : String {
+        return "";
+    }
+
+    /** Maps the specified relative or virtual path to the corresponding physical directory on the server. **/
+    public function mapPath(input : String) : String {
+        return "";
+    }
+
+    /** Applies URL encoding rules, including escape characters, to the specified string. **/
+    public function URLEncode(input : String) : String {
+        return "";
+    }
+}
