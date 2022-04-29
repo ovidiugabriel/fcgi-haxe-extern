ce0c724fa3d51cf56efdf4ad5736ccfa82b4bcf4
Ovidiu <ovidiugabriel@gmail.com>
2022-04-29 17:17:33 +0300

    Update Cookie.hx

--- haxe/web/cgi/Cookie.hx
+++ haxe/web/cgi/Cookie.hx
@@ -11,78 +11,82 @@ class Cookie {
     private var sameSite : String;
     private var httpOnly : Bool;
 
-    //  Retrieve the Domain attribute of the cookie.
+    /**  Retrieve the Domain attribute of the cookie. **/
     public function getDomain() : String {
         return this.domain;
     }
 
-    //  Retrieve maximum age of the cookie.
+    /**  Retrieve maximum age of the cookie. **/
     public function getMaxAge() : Int {
         return this.maxAge;
     }
 
-    //  Retrieve the name of the cookie.
+    /**  Retrieve the name of the cookie. **/
     public function getName() : String {
         return this.name;
     }
 
-    //  Retrieve the path on the server to which browser returns the cookie.
+    /**  Retrieve the path on the server to which browser returns the cookie. **/
     public function getPath() : String {
         return this.path;
     }
 
-    //  determine if the browser is sending the cookie only over a secure protocol.
+    /** Get the Secure attribute of the cookie. **/
     public function getSecure() : Bool {
         return this.secure;
     }
 
-    //  Retrieve the value of the cookie.
+    /**  Retrieve the value of the cookie. **/
     public function getValue() : String {
         return this.value;
     }
 
-    // Set the Domain attribute of the cookie.
+    /** Set the Domain attribute of the cookie. **/
     public function setDomain(domain : String) : Void {
         this.domain = domain;
     }
 
-    // Set maximum age of the cookie.
+    /** Set maximum age of the cookie. **/
     public function setMaxAge(maxAge : Int) : Void {
         this.maxAge = maxAge;
     }
 
-    // Set the name of the cookie.
+    /** Set the name of the cookie. **/
     public function setName(name: String) : Void {
         this.name = name;
     }
 
-    //  Set the path on the server to which browser returns the cookie.
+    /**  Set the path on the server to which browser returns the cookie. **/
     public function setPath(path : String) : Void {
         this.path = path;
     }
 
-    // Set the Secure attribute of the cookie.
+    /** Set the Secure attribute of the cookie. **/
     public function setSecure(secure : Bool) : Void {
         this.secure = secure;
     }
 
-    // Set the value of the cookie.
+    /** Set the value of the cookie. **/
     public function setValue(value : String) : Void {
         this.value = value;
     }
 
+    /** Set the SameSite attribute of the cookie. **/
     public function setSameSite(sameSite : String) : Void {
         this.sameSite = sameSite;
     }
 
+    /** Get the SameSite attribute of the cookie. **/
     public function getSameSite() : String {
         return this.sameSite;
     }
 
+    /** Set the HttpOnly attribute of the cookie. **/
     public function setHttpOnly(httpOnly : Bool) : Void {
         this.httpOnly = httpOnly;
     }
 
+    /** Get the HttpOnly attribute of the cookie. **/
     public function getHttpOnly() : Bool {
         return this.httpOnly;
     }
