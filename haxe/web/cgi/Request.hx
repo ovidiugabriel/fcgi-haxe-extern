16effa630b64162d3e722f45e86a9ae9b04f7746
Ovidiu <ovidiugabriel@gmail.com>
2022-05-02 14:34:45 +0300

    Update Request.hx

--- haxe/web/cgi/Request.hx
+++ haxe/web/cgi/Request.hx
@@ -4,48 +4,28 @@ package web.cgi;
 import haxe.io.BytesData;
 import haxe.ds.StringMap;
 
-class Request {
-    /**
-        Retrieves the bytes that were read by an HTTP POST.
-     **/
-    public function binaryRead(count : Int) : BytesData {
-        return new BytesData();
-    }
+extern class Request {
+    /** Retrieves the bytes that were read by an HTTP POST. **/
+    public function binaryRead(count : Int) : BytesData ;
 
-    /**
-        Retrieves a cookie value by its name.
-     **/
-    public function getCookie() : String {
-        return "";
-    }
+    /** Retrieves a cookie value by its name. **/
+    public function getCookie() : String ;
 
     /** Retrieves all cookies. **/
-    public function getCookies() : Array<Cookie> {
-        return new Array<Cookie>();
-    }
+    public function getCookies() : Array<Cookie> ;
 
     /** Retrieves the value of a specified name which was read by POST or GET method. **/
-    public function getForm(name : String) : String {
-        return "";
-    }
+    public function getForm(name : String) : String ;
 
     /** Retrieves all values of a specified name which were read by POST or GET method. **/
-    public function getForms(name : String) : Array<String> {
-        return new Array<String>();
-    }
+    public function getForms(name : String) : Array<String> ;
 
     /** Retrieves all pairs of name and value that were read by POST or GET method. **/
-    public function getFormNameValue() : StringMap<String> {
-        return new StringMap();
-    }
+    public function getFormNameValue() : StringMap<String> ;
 
     /** Retrieves a specified ServerVariable value. **/
-    public function getServerVariable(variableName : String) : String {
-        return "";
-    }
+    public function getServerVariable(variableName : String) : String ;
 
     /** Returns the size, in bytes, of the current request. **/
-    public function getTotalBytes() : Int {
-        return 0;
-    }
+    public function getTotalBytes() : Int ;
 }
